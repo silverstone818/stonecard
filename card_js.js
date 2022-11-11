ff3c9b2ee432592cdb97c5a488d5e30d8fa0af16
silverstone818 <107938579+silverstone818@users.noreply.github.com>
2022-11-11 15:14:43 +0900

    Add files via upload

---
+++ card_js.js
@@ -0,0 +1,554 @@
+let card = new Array();
+
+//클로버 구간
+
+//클로버 A
+card[0] = new Image();
+card[0].src = "img/c_a.png";
+card[0].name = "클로버 A"
+card[0].value = 1;
+//클로버 2
+card[1] = new Image();
+card[1].src = "img/c_2.png";
+card[1].name = "클로버 2"
+card[1].value = 2;
+//클로버 3
+card[2] = new Image();
+card[2].src = "img/c_3.png";
+card[2].name = "클로버 3"
+card[2].value = 3;
+//클로버 4
+card[3] = new Image();
+card[3].src = "img/c_4.png";
+card[3].name = "클로버 4"
+card[3].value = 4;
+//클로버 5
+card[4] = new Image();
+card[4].src = "img/c_5.png";
+card[4].name = "클로버 5"
+card[4].value = 5;
+//클로버 6
+card[5] = new Image();
+card[5].src = "img/c_6.png";
+card[5].name = "클로버 6"
+card[5].value = 6;
+//클로버 7
+card[6] = new Image();
+card[6].src = "img/c_7.png";
+card[6].name = "클로버 7"
+card[6].value = 7;
+//클로버 8
+card[7] = new Image();
+card[7].src = "img/c_8.png";
+card[7].name = "클로버 8"
+card[7].value = 8;
+//클로버 9
+card[8] = new Image();
+card[8].src = "img/c_9.png";
+card[8].name = "클로버 9"
+card[8].value = 9;
+//클로버 10
+card[9] = new Image();
+card[9].src = "img/c_10.png";
+card[9].name = "클로버 10"
+card[9].value = 10;
+//클로버 J
+card[10] = new Image();
+card[10].src = "img/c_j.png";
+card[10].name = "클로버 J"
+card[10].value = 11;
+//클로버 Q
+card[11] = new Image();
+card[11].src = "img/c_q.png";
+card[11].name = "클로버 Q"
+card[11].value = 12;
+//클로버 K
+card[12] = new Image();
+card[12].src = "img/c_k.png";
+card[12].name = "클로버 K"
+card[12].value = 13;
+
+//하트 구간
+
+//하트 A
+card[13] = new Image();
+card[13].src = "img/h_a.png";
+card[13].name = "하트 A"
+card[13].value = 1;
+//하트 2
+card[14] = new Image();
+card[14].src = "img/h_2.png";
+card[14].name = "하트 2"
+card[14].value = 2;
+//하트 3
+card[15] = new Image();
+card[15].src = "img/h_3.png";
+card[15].name = "하트 3"
+card[15].value = 3;
+//하트 4
+card[16] = new Image();
+card[16].src = "img/h_4.png";
+card[16].name = "하트 4"
+card[16].value = 4;
+//하트 5
+card[17] = new Image();
+card[17].src = "img/h_5.png";
+card[17].name = "하트 5"
+card[17].value = 5;
+//하트 6
+card[18] = new Image();
+card[18].src = "img/h_6.png";
+card[18].name = "하트 6"
+card[18].value = 6;
+//하트 7
+card[19] = new Image();
+card[19].src = "img/h_7.png";
+card[19].name = "하트 7"
+card[19].value = 7;
+//하트 8
+card[20] = new Image();
+card[20].src = "img/h_8.png";
+card[20].name = "하트 8"
+card[20].value = 8;
+//하트 9
+card[21] = new Image();
+card[21].src = "img/h_9.png";
+card[21].name = "하트 9"
+card[21].value = 9;
+//하트 10
+card[22] = new Image();
+card[22].src = "img/h_10.png";
+card[22].name = "하트 10"
+card[22].value = 10;
+//하트 J
+card[23] = new Image();
+card[23].src = "img/h_j.png";
+card[23].name = "하트 J"
+card[23].value = 11;
+//하트 Q
+card[24] = new Image();
+card[24].src = "img/h_q.png";
+card[24].name = "하트 Q"
+card[24].value = 12;
+//하트 K
+card[25] = new Image();
+card[25].src = "img/h_k.png";
+card[25].name = "하트 K"
+card[25].value = 13;
+
+//다이아 구간
+
+//다이아 A
+card[26] = new Image();
+card[26].src = "img/d_a.png";
+card[26].name = "다이아 A"
+card[26].value = 1;
+//다이아 2
+card[27] = new Image();
+card[27].src = "img/d_2.png";
+card[27].name = "다이아 2"
+card[27].value = 2;
+//다이아 3
+card[28] = new Image();
+card[28].src = "img/d_3.png";
+card[28].name = "다이아 3"
+card[28].value = 3;
+//다이아 4
+card[29] = new Image();
+card[29].src = "img/d_4.png";
+card[29].name = "다이아 4"
+card[29].value = 4;
+//다이아 5
+card[30] = new Image();
+card[30].src = "img/d_5.png";
+card[30].name = "다이아 5"
+card[30].value = 5;
+//다이아 6
+card[31] = new Image();
+card[31].src = "img/d_6.png";
+card[31].name = "다이아 6"
+card[31].value = 6;
+//다이아 7
+card[32] = new Image();
+card[32].src = "img/d_7.png";
+card[32].name = "다이아 7"
+card[32].value = 7;
+//다이아 8
+card[33] = new Image();
+card[33].src = "img/d_8.png";
+card[33].name = "다이아 8"
+card[33].value = 8;
+//다이아 9
+card[34] = new Image();
+card[34].src = "img/d_9.png";
+card[34].name = "다이아 9"
+card[34].value = 9;
+//다이아 10
+card[35] = new Image();
+card[35].src = "img/d_10.png";
+card[35].name = "다이아 10"
+card[35].value = 10;
+//다이아 J
+card[36] = new Image();
+card[36].src = "img/d_j.png";
+card[36].name = "다이아 J"
+card[36].value = 11;
+//다이아 Q
+card[37] = new Image();
+card[37].src = "img/d_q.png";
+card[37].name = "다이아 Q"
+card[37].value = 12;
+//다이아 K
+card[38] = new Image();
+card[38].src = "img/d_k.png";
+card[38].name = "다이아 K"
+card[38].value = 13;
+
+//스페이드 구간
+
+//스페이드 A
+card[39] = new Image();
+card[39].src = "img/s_a.png";
+card[39].name = "스페이드 A"
+card[39].value = 1;
+//스페이드 2
+card[40] = new Image();
+card[40].src = "img/s_2.png";
+card[40].name = "스페이드 2"
+card[40].value = 2;
+//스페이드 3
+card[41] = new Image();
+card[41].src = "img/s_3.png";
+card[41].name = "스페이드 3"
+card[41].value = 3;
+//스페이드 4
+card[42] = new Image();
+card[42].src = "img/s_4.png";
+card[42].name = "스페이드 4"
+card[42].value = 4;
+//스페이드 5
+card[43] = new Image();
+card[43].src = "img/s_5.png";
+card[43].name = "스페이드 5"
+card[43].value = 5;
+//스페이드 6
+card[44] = new Image();
+card[44].src = "img/s_6.png";
+card[44].name = "스페이드 6"
+card[44].value = 6;
+//스페이드 7
+card[45] = new Image();
+card[45].src = "img/s_7.png";
+card[45].name = "스페이드 7"
+card[45].value = 7;
+//스페이드 8
+card[46] = new Image();
+card[46].src = "img/s_8.png";
+card[46].name = "스페이드 8"
+card[46].value = 8;
+//스페이드 9
+card[47] = new Image();
+card[47].src = "img/s_9.png";
+card[47].name = "스페이드 9"
+card[47].value = 9;
+//스페이드 10
+card[48] = new Image();
+card[48].src = "img/s_10.png";
+card[48].name = "스페이드 10"
+card[48].value = 10;
+//스페이드 J
+card[49] = new Image();
+card[49].src = "img/s_j.png";
+card[49].name = "스페이드 J"
+card[49].value = 11;
+//스페이드 Q
+card[50] = new Image();
+card[50].src = "img/s_q.png";
+card[50].name = "스페이드 Q"
+card[50].value = 12;
+//스페이드 K
+card[51] = new Image();
+card[51].src = "img/s_k.png";
+card[51].name = "스페이드 K"
+card[51].value = 13;
+
+let player_card = new Array();
+player_card[0] = new Image();
+player_card[0].src = "";
+player_card[0].name = ""
+player_card[0].value = 0;
+player_card[1] = new Image();
+player_card[1].src = "";
+player_card[1].name = ""
+player_card[1].value = 0;
+player_card[2] = new Image();
+player_card[2].src = "";
+player_card[2].name = ""
+player_card[2].value = 0;
+
+let banker_card = new Array();
+banker_card[0] = new Image();
+banker_card[0].src = "";
+banker_card[0].name = ""
+banker_card[0].value = 0;
+banker_card[1] = new Image();
+banker_card[1].src = "";
+banker_card[1].name = ""
+banker_card[1].value = 0;
+banker_card[2] = new Image();
+banker_card[2].src = "";
+banker_card[2].name = ""
+banker_card[2].value = 0;
+
+let count = 1;
+let playerScore = 0;
+let bankerScore = 0;
+let money = 100000;
+
+function Deel(){
+  let p_cell = document.getElementById('p_cell');
+  let b_cell = document.getElementById('b_cell');
+  let p_score = document.getElementById('p_score');
+  let b_score = document.getElementById('b_score');
+  let p_win = document.getElementById('p_win').value;
+  let b_win = document.getElementById('b_win').value;
+  let p_pair = document.getElementById('p_pair').value;
+  let b_pair = document.getElementById('b_pair').value;
+  let tie = document.getElementById('tie').value;
+  let p_money = document.getElementById('p_money');
+
+  if(money <= 0){
+    btn.disabled = true;
+    return;
+  }
+  if(tie <= 0 && b_pair <= 0 && p_pair <= 0 && b_win <= 0 && p_win <= 0){
+    alert('돈을 투자하십시오.');
+    return;
+  }
+  let btn = document.querySelector('.btn');
+  
+  btn.disabled = true;
+
+  money += -p_win -b_win -p_pair -b_pair -tie;
+  p_money.innerText = `${money}`
+
+  playerScore = 0;
+  bankerScore = 0;
+  p_score.innerText = `${playerScore}`;
+  b_score.innerText = `${bankerScore}`;
+  count = 1;  
+  
+  for(let i = 0; i < 2; i++){
+    p_create(i, i * 37);
+    b_create(i, i * 37);
+  }
+
+  if(player_card[0].value >= 10) {playerScore += 0;}
+  else {playerScore += player_card[0].value;}
+  if(player_card[1].value >= 10) {playerScore += 0;}
+  else {playerScore += player_card[1].value;}
+  if(banker_card[0].value >= 10){bankerScore += 0;}
+  else {bankerScore += banker_card[0].value;}
+  if(banker_card[1].value >= 10) {bankerScore += 0;}
+  else {bankerScore += banker_card[1].value;}
+  
+  playerScore %= 10;
+  bankerScore %= 10;
+
+  setTimeout(function() {
+    p_score.innerText = `${playerScore}`;
+    b_score.innerText = `${bankerScore}`;
+  }, count++*500);
+
+  playerRule(playerScore, bankerScore);
+  bankerRule(playerScore, bankerScore);
+  
+  let playerScore2 = playerScore;
+  let bankerScore2 = bankerScore;
+
+  if(player_card[2].value >= 10) {playerScore2 += 0;}
+  else {playerScore2 += player_card[2].value;}
+  if(banker_card[2].value >= 10) {bankerScore2 += 0;}
+  else {bankerScore2 += banker_card[2].value;}
+
+  playerScore2 %= 10;
+  bankerScore2 %= 10;
+  
+  setTimeout(function() {
+    p_score.innerText = `${playerScore2}`;
+    b_score.innerText = `${bankerScore2}`;
+  }, count++*500);
+
+  setTimeout(function(){
+    Batting(playerScore2, bankerScore2, p_win, b_win, p_pair, b_pair, tie);
+  }, count++*500);
+  
+  setTimeout(function(){
+    if(money <= 0){
+      alert('파산하셨습니다. 다시 시작하실려면 새로고침 해주십시오.');
+      return;
+    }
+    while ( p_cell.hasChildNodes() )
+    {
+      p_cell.removeChild( p_cell.firstChild );  //플레이어 필드의 카드 제거
+    }
+    while ( b_cell.hasChildNodes() )
+    {
+      b_cell.removeChild( b_cell.firstChild );  //플레이어 필드의 카드 제거
+    }
+    p_score.innerText = `0`;
+    b_score.innerText = `0`;
+  }, count++*500);
+  
+  setTimeout(function() {
+    p_money.innerText = `${money}`;
+    btn.disabled = false;
+  }, count*500);
+}
+
+function Batting(p_score, b_score, p_win, b_win, p_pair, b_pair, tie){
+  if(p_score > b_score){
+    money += p_win * 2;
+  }
+  if(p_score < b_score){
+    money += b_win * 2;
+  }
+  if(p_score == b_score){
+    money += tie * 8;
+  }
+  if(player_card[0].value == player_card[1].value || player_card[0].value == player_card[2].value || player_card[1].value == player_card[2].value){
+    money += p_pair * 11;
+  }
+  if(banker_card[0].value == banker_card[1].value || banker_card[0].value == banker_card[2].value || banker_card[1].value == banker_card[2].value){
+    money += b_pair * 11;
+  }
+}
+
+function playerRule(p, b){
+  if(b == 8 || b == 9){
+    player_card[2].value = 0;
+    banker_card[2].value = 0;
+    return;
+  }
+  else if(p == 8 || p == 9){
+    player_card[2].value = 0;
+    banker_card[2].value = 0;
+    return;
+  }
+  else if(b == 7){
+    player_card[2].value = 0;
+    return;
+  }
+  else if(p == 6 || p == 7){
+    player_card[2].value = 0;
+    return;
+  }
+  else if(p >= 0 && p <= 5){
+    p_create(2, 2 * 37);
+    return;
+  }
+}
+
+function bankerRule(p, b){
+  if(p == 8 || p == 9){
+    player_card[2].value = 0;
+    banker_card[2].value = 0;
+    return;
+  }
+  else if(b == 8 || b == 9){
+    player_card[2].value = 0;
+    banker_card[2].value = 0;
+    return;
+  }
+  else if(b == 3){
+    if(player_card[2].value == 8){
+      banker_card[2].value = 0;
+      return;
+    }
+    else{
+      b_create(2, 2 * 37);
+      return;
+    }
+  }
+  else if(b == 4){
+    if(player_card[2].value > 1 && player_card[2].value < 8){
+      b_create(2, 2 * 37);
+      return;
+    }
+    else{
+      banker_card[2].value = 0;
+      return;
+    }
+  }
+  else if(b == 5){
+    if(player_card[2].value > 3 && player_card[2].value < 8){
+      b_create(2, 2 * 37);
+      return;
+    }
+    else{
+      banker_card[2].value = 0;
+      return;
+    }
+  }
+  else{
+    b_create(2, 2 * 37);
+    return;
+  }
+}
+function p_create(sos, pos){
+  let p_cell = document.getElementById('p_cell');
+  let player = document.createElement('inline');
+
+  p_card(sos);
+
+  player.setAttribute('id', 'p_card');
+  player.style.backgroundImage = `url('${player_card[sos].src}')`;
+  player.style.backgroundSize = "20vmin 28vmin";
+  player.style.left = `${pos}%`;
+
+  setTimeout(function() {
+    p_cell.appendChild(player);
+  }, count++*500);
+  
+}
+
+function b_create(sos, pos){
+  let b_cell = document.getElementById('b_cell');
+  let banker = document.createElement('inline');
+  b_card(sos);
+
+  banker.setAttribute('id', 'b_card');
+  banker.style.backgroundImage = `url('${banker_card[sos].src}')`;
+  banker.style.backgroundSize = "20vmin 28vmin";
+  banker.style.left = `${pos}%`;
+
+  setTimeout(function() {
+    b_cell.appendChild(banker);
+  }, count++*500);
+}
+
+function randomCard(){
+  let rand_0_52 = Math.floor(Math.random() * 52)
+  for(let i = 0; i < 3; i++){
+    if(card[rand_0_52] == player_card[i]){
+      rand_0_52 = Math.floor(Math.random() * 52)
+      i = -1;
+      continue;
+    }
+    if(card[rand_0_52] == banker_card[i]){
+      rand_0_52 = Math.floor(Math.random() * 52)
+      i = -1;
+      continue;
+    }
+  }
+  return rand_0_52;
+}
+
+function p_card(sos){
+  let rand_0_52 = randomCard();
+  player_card[sos] = card[rand_0_52];
+}
+function b_card(sos){
+  let rand_0_52 = randomCard();
+  banker_card[sos] = card[rand_0_52];
+}
